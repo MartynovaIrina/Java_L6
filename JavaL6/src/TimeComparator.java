7fa82a1c5bf61c33c2759ce7900ede0fb39e8464
Irina <irinamartynova92@gmail.com>
2022-08-29 18:04:32 +0300

    compare changed

--- JavaL6/src/TimeComparator.java
+++ JavaL6/src/TimeComparator.java
@@ -1,18 +1,23 @@
 import java.util.Comparator;
 
 public class TimeComparator implements Comparator<Time> {
+    int totalTimeSecTime1;
+    int totalTimeSecTime2;
     @Override
     public int compare(Time time1, Time time2){
-        if (time1.getHours() > time2.getHours()){
+        totalTimeSecTime1 = convertToSec(time1.getHours(), time1.getMinutes(), time1.getSeconds());
+        totalTimeSecTime2 = convertToSec(time2.getHours(), time2.getMinutes(), time2.getSeconds());
+
+        if (totalTimeSecTime1 > totalTimeSecTime2){
             return 1;
-        } else if (time1.getHours() == time2.getHours() && time1.getMinutes() > time2.getMinutes()) {
-            return 1;
-        } else if (time1.getHours() == time2.getHours() && time1.getMinutes() == time2.getMinutes() &&
-                time1.getSeconds() > time2.getSeconds()) {
-            return 1;
-        }
-        else{
+        } else if (totalTimeSecTime1 < totalTimeSecTime2) {
             return -1;
         }
+        else{
+            return 0;
+        }
+    }
+    private int convertToSec(int hours, int minutes, int seconds){
+        return hours * 3600 + minutes * 60 + seconds;
     }
 }
